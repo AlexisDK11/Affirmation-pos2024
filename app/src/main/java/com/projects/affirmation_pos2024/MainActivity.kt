2ddcef66931ee5c418a2eea84f46b31a9589b998
AlexisDK11 <kang.consertos@gmail.com>
2024-08-25 17:25:49 -0300

    finished project

--- app/src/main/java/com/projects/affirmation_pos2024/MainActivity.kt
+++ app/src/main/java/com/projects/affirmation_pos2024/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.projects.affirmation_pos2024
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.projects.affirmation_pos2024.adapter.ItemAdapter
+import com.projects.affirmation_pos2024.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerview  = findViewById<RecyclerView>(R.id.recycler_view)
+
+        val myDataset = DataSource().loadAffirmation()
+
+
+
+        recyclerview.adapter = ItemAdapter(this, myDataset)
+
     }
 }
---
+++ app/src/main/java/com/projects/affirmation_pos2024/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.projects.affirmation_pos2024.model
+
+data class Affirmation(var stringResourceId:Int)
